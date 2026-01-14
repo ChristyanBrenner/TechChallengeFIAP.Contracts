61fef141084f5336f334a6bed0ec6550baeecb1b
Christyan Souza <christyan.souza@datapar.com.br>
2026-01-14 14:41:55 -0300

    Initial commit

---
+++ CloudGames.Contracts/Events/OrderPlacedEvent.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CloudGames.Contracts.Events
+{
+    public record OrderPlacedEvent(
+    int Id,
+    int UserId,
+    int GameId,
+    string GameName,
+    decimal Price
+);
+}
---
+++ CloudGames.Contracts/Events/PaymentProcessedEvent.cs
@@ -0,0 +1,15 @@
+﻿namespace CloudGames.Contracts.Events
+{
+    public record PaymentProcessedEvent(
+    int UserId,
+    int GameId,
+    string GameName,
+    PaymentStatus Status
+);
+
+    public enum PaymentStatus
+    {
+        Approved,
+        Rejected
+    }
+}
